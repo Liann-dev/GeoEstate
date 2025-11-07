ae307b4b14c479d416c81e3334a6d7744a8d82ed
NaufalSalva <mhmmdnaufalh@gmail.com>
2025-11-07 16:07:28 +0700

    First commit add Auth (Login & Register)

---
+++ main.py
@@ -0,0 +1,26 @@
+from app.auth.register import register
+from app.auth.login import login
+
+
+def main():
+    while True:
+        print("=== GeoEstate ===")
+        print("1. Register")
+        print("2. Login")
+        print("3. Exit")
+        pilihan = input("Pilih opsi (1/2/3): ")
+
+        if pilihan == '1':
+            register()
+        elif pilihan == '2':
+            if login():
+                print("Selamat datang di GeoEstate!")
+                break
+        elif pilihan == '3':
+            print("Terima kasih telah menggunakan GeoEstate!")
+            break
+        else:
+            print("Opsi tidak valid, silakan coba lagi.\n")
+
+if __name__ == "__main__":
+    main()
